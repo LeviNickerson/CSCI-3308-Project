d3262e5f1f2c854db3ae3f699af4d77fb2944726
jteufel <36942884+jteufel@users.noreply.github.com>
2018-12-08 16:58:37 -0700

    Front End Files

---
+++ Front_End_Files/crime.js
@@ -0,0 +1,40 @@
+var state = 1;
+var n_button_bool = false;
+var c_button_bool = false;
+
+var n_button_selected = "";
+var c_button_selected = "";
+
+
+function change_n_Selection(btn) {
+    var n_button = document.getElementById(btn);
+      if (n_button_bool == false) {
+          n_button.style.backgroundColor = "#0066ff"
+          n_button_bool = true;
+          n_button_selected = n_button
+      }
+      else {
+
+          n_button_selected.style.backgroundColor = "#eee"
+          n_button.style.backgroundColor = "#0066ff"
+          n_button_selected = n_button
+
+        }
+   }
+
+function change_c_Selection(btn) {
+
+    var c_button = document.getElementById(btn);
+        if (c_button_bool == false) {
+            c_button.style.backgroundColor = "#0066ff"
+            c_button_bool = true;
+            c_button_selected = c_button
+         }
+        else {
+
+            c_button_selected.style.backgroundColor = "#eee"
+            c_button.style.backgroundColor = "#0066ff"
+            c_button_selected = c_button
+
+           }
+      }
