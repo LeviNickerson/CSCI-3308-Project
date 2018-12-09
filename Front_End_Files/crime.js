e0a30c5ec6c264060eba1f645664183a3e67b3fc
jteufel <36942884+jteufel@users.noreply.github.com>
2018-12-08 23:02:15 -0700

    JS Update to include JSON text

--- Front_End_Files/crime.js
+++ Front_End_Files/crime.js
@@ -5,6 +5,39 @@ var c_button_bool = false;
 var n_button_selected = "";
 var c_button_selected = "";
 
+var n_input= "";
+var c_input= "";
+
+var data = {
+"nieghboorhood_one":{
+  "Crime_One":"This is the data on crime one in neighboorhood one",
+  "Crime_Two":"This is the data on crime two in neighboorhood one",
+  "Crime_Three":"This is the data on crime one in neighboorhood one"
+},
+"nieghboorhood_two":{
+  "Crime_One":"This is the data on crime one in neighboorhood two",
+  "Crime_Two":"This is the data on crime two in neighboorhood two",
+  "Crime_Three":"This is the data on crime one in neighboorhood two"
+},
+"nieghboorhood_three":{
+  "Crime_One":"This is the data on crime one in neighboorhood three",
+  "Crime_Two":"This is the data on crime two in neighboorhood three",
+  "Crime_Three":"This is the data on crime one in neighboorhood three"
+},
+"nieghboorhood_four":{
+  "Crime_One":"This is the data on crime one in neighboorhood four",
+  "Crime_Two":"This is the data on crime two in neighboorhood four",
+  "Crime_Three":"This is the data on crime one in neighboorhood four"
+},
+"nieghboorhood_five":{
+  "Crime_One":"This is the data on crime one in neighboorhood five",
+  "Crime_Two":"This is the data on crime two in neighboorhood five",
+  "Crime_Three":"This is the data on crime one in neighboorhood five"
+}
+
+};
+
+
 
 function change_n_Selection(btn) {
     var n_button = document.getElementById(btn);
@@ -43,7 +76,13 @@ function return_data() {
 
     if (n_button_bool == true && c_button_bool == true){
 
-      document.getElementById('display_text').innerHTML = "SUCCESS"
+      var neighboorhood_input = ""
+      var crime_input = ""
+
+      var neighboorhood_data = data[n_input]
+      var crime_data = neighboorhood_data["Crime_One"]
+
+      document.getElementById('display_text').innerHTML = crime_data
 
     }else{
 
@@ -53,9 +92,14 @@ function return_data() {
 
     }
 
+function set_n_input(input_string) {
 
-function print_results() {
+  n_input= input_string;
 
-  document.getElementById('display_text').innerHTML = "SUCCESS"
+}
 
-                        }
+function set_c_input(input_string) {
+
+  c_input= input_string;
+
+}
